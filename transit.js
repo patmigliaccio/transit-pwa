1262733c4326d5803f6cef6596643bb913d04b43
Pat Migliaccio <pat@patmigliaccio.com>
2017-05-24 22:11:05 -0400

    add basic cheerio html parsing

--- transit.js
+++ transit.js
@@ -1,15 +1,25 @@
-const axios = require('axios'),
+const fs = require('fs')
+		axios = require('axios'),
 		qs	= require('qs'),
-		fs = require('fs');
+		cheerio = require('cheerio');
 
 const CONFIG = require('./config'); 
 
 axios.post(CONFIG.host, qs.stringify(CONFIG.options))
 	.then(response => parse(response.data))
-	.then(write)
+	//.then(write)
 	.catch(failure);
 
 function parse(data) {
+	let $ = cheerio.load(data);
+
+	for (selector in CONFIG.selectors){
+		if (CONFIG.selectors[selector]){
+			let value = $(CONFIG.selectors[selector]).text();
+			console.log(value);	
+		}
+	}
+
 	return data;
 }
 
